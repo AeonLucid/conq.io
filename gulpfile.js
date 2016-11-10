5d76946d5ce6121e558d9043b7cab3dfdeaacf67
Shashi Chotkan <ssr.dev@hotmail.com>
2016-11-10 15:54:04 +0100

    Enhance watcher with client/server synchronization
    The gulp task runner now use nodemon to restart the server on file
    change. After running the server, browser sync will be called to
    automatically refresh the page to stay synchronized.
    
    This cycle greatly improves productivity as the tedious work such as
    running the script and opening the browser completely got eliminated.

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var uglify_css = require('gulp-clean-css');
 var uglify_html = require('gulp-htmlmin');
+var browser_sync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+var reload = browser_sync.reload;
 
 var tsconfig = {
     target: "es2015",
@@ -25,27 +29,27 @@ var bbonfig = {
 gulp.task("default", ["rebuild"]);
 
 // Compile and uglify typescript code for client
-gulp.task("build-client", function() {
+gulp.task("build-client", function () {
     var res = gulp.src("src/public/**/*.ts")
-        .pipe(gts(tsconfig))
-        .pipe(babel(bbonfig))
         .pipe(order([
             "src/public/!(start)*.js",
             "src/public/start.js"
         ], { base: './' }))
         .pipe(print())
-        .pipe(concat("script.js"))
-        .pipe(uglify())
+        .pipe(concat("script.ts"))
+        .pipe(gts(tsconfig))
+        //.pipe(babel(bbonfig))
+        //.pipe(uglify())
         .pipe(gulp.dest("dist/public"));
     return res;
 });
 
 // Compile and uglify typescript code for server
-gulp.task("build-server", function() {
+gulp.task("build-server", function () {
     var res = gulp.src("src/server/**/*.ts")
         .pipe(gts(tsconfig))
-        .pipe(babel(bbonfig))
-        .pipe(uglify())
+        //.pipe(babel(bbonfig))
+        //.pipe(uglify())
         .pipe(gulp.dest("dist/server"));
     return res;
 });
@@ -54,7 +58,7 @@ gulp.task("build-server", function() {
 gulp.task("build-view", ["build-html", "build-css"]);
 
 // Copy and uglify html code for client
-gulp.task("build-html", function() {
+gulp.task("build-html", function () {
     var settings = {
         collapseWhitespace: true,
         removeComments: true
@@ -66,9 +70,8 @@ gulp.task("build-html", function() {
     return res;
 });
 
-
 // Copy and uglify css code for client
-gulp.task("build-css", function() {
+gulp.task("build-css", function () {
     var res = gulp.src("src/view/**/*.css")
         .pipe(uglify_css())
         .pipe(gulp.dest("dist/public"));
@@ -79,19 +82,50 @@ gulp.task("build-css", function() {
 gulp.task("build", ["build-client", "build-server", "build-view"]);
 
 // Clean the build folder
-gulp.task("clean", function() {
+gulp.task("clean", function () {
     var res = del(["dist/**/*"]);
     return res;
 });
 
 // Clean the build folder and rebuild the applicaton
-gulp.task("rebuild", function() {
+gulp.task("rebuild", function () {
     var res = run_sequence('clean', 'build');
+    return res;
 });
 
+// Initializes watchers for automatically building files, restarting
+// the server and reloading the page.
+gulp.task("watch", ["nodemon-init", "browser-sync-init", "watch-files"]);
+
 // Call the appropriate build task when the working directory change
-gulp.task("watch", function() {
+gulp.task("watch-files", function () {
     gulp.watch("src/public/**/*.ts", ["build-client"]);
     gulp.watch("src/server/**/*.ts", ["build-server"]);
     gulp.watch("src/view/**/*.*", ["build-view"]);
 });
+
+// Reloads the browsers on file change.
+gulp.task("browser-sync-init", function () {
+    var res = browser_sync({
+        proxy: "localhost:3000",
+        port: 5000,
+        notify: true
+    });
+    return res;
+});
+
+// Restarts the server on file change.
+gulp.task("nodemon-init", function () {
+    var res = nodemon({
+        script: "dist/server/init.js",
+        watch: "src/**/*.*"
+    });
+
+    res.on("start", function () {
+        setTimeout(function () {
+            reload({ stream: false });
+        }, 1000);
+    });
+
+    return res;
+})
