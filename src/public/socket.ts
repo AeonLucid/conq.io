4eff55a30d700fcb0d6cd7fee05ac8c40483362d
Shashi Chotkan <ssr.dev@hotmail.com>
2016-11-10 23:49:36 +0100

    Put client interface inside namespace
    As the browser code has no module system, where all files are concatenated
    into one file, it is important to not pollute the global scope with
    symbols, hence the need of a namespace.

--- src/public/socket.ts
+++ src/public/socket.ts
@@ -1,182 +1,184 @@
-type Binary = ArrayBuffer;
-type PacketReceiver = (packet: Packet) => void;
-let padding = 2;
+namespace wsw {
+    export type Binary = ArrayBuffer;
+    export let padding = 2;
 
-type StateListener = () => void;
+    type StateListener = () => void;
+    type PacketReceiver = (packet: Packet) => void;
 
-interface Packet {
-    data: Binary;
-}
-
-interface ResolverManagerObject {
-    main: number;
-    sub: number;
-    receiver: PacketReceiver;
-}
-
-class ResolverManager {
-    private m_resolvers: ResolverManagerObject[];
-
-    constructor() {
-        this.m_resolvers = [];
+    interface Packet {
+        data: Binary;
     }
 
-    public set(main: number, sub: number, receiver: PacketReceiver) {
-        for (let resolver of this.m_resolvers) {
-            if (resolver.main === main && resolver.sub === sub) {
-                resolver.receiver = receiver;
-                return;
+    interface ResolverManagerObject {
+        main: number;
+        sub: number;
+        receiver: PacketReceiver;
+    }
+
+    class ResolverManager {
+        private m_resolvers: ResolverManagerObject[];
+
+        constructor() {
+            this.m_resolvers = [];
+        }
+
+        public set(main: number, sub: number, receiver: PacketReceiver) {
+            for (let resolver of this.m_resolvers) {
+                if (resolver.main === main && resolver.sub === sub) {
+                    resolver.receiver = receiver;
+                    return;
+                }
+            }
+
+            this.m_resolvers.push({ main: main, sub: sub, receiver: receiver });
+        }
+
+        public unset(main: number, sub: number) {
+            for (let i = 0; i < this.m_resolvers.length; i++) {
+                let resolver = this.m_resolvers[i];
+
+                if (resolver.main === main && resolver.sub === sub) {
+                    this.m_resolvers.splice(i, 1);
+                    return;
+                }
             }
         }
 
-        this.m_resolvers.push({ main: main, sub: sub, receiver: receiver });
-    }
-
-    public unset(main: number, sub: number) {
-        for (let i = 0; i < this.m_resolvers.length; i++) {
-            let resolver = this.m_resolvers[i];
-
-            if (resolver.main === main && resolver.sub === sub) {
-                this.m_resolvers.splice(i, 1);
-                return;
+        public call(main: number, sub: number, packet: Packet) {
+            for (let resolver of this.m_resolvers) {
+                if (resolver.main === main && resolver.sub === sub) {
+                    resolver.receiver(packet);
+                }
             }
         }
     }
 
-    public call(main: number, sub: number, packet: Packet) {
-        for (let resolver of this.m_resolvers) {
-            if (resolver.main === main && resolver.sub === sub) {
-                resolver.receiver(packet);
+    interface ResolverStateWrapper {
+        listener: StateListener;
+    }
+
+    class ResolverStateBinder {
+        private m_stateListener: ResolverStateWrapper;
+
+        constructor(stateListener: ResolverStateWrapper) {
+            this.m_stateListener = stateListener;
+        }
+
+        public do(callback: StateListener) {
+            this.m_stateListener.listener = callback;
+        }
+
+        public ignore() {
+            this.m_stateListener.listener = () => { };
+        }
+    }
+
+    class ResolverBinder {
+        private m_manager: ResolverManager;
+        private m_main: number;
+        private m_subs: number[];
+
+        constructor(manager: ResolverManager, main: number, subs: number[]) {
+            this.m_manager = manager;
+            this.m_main = main;
+            this.m_subs = subs;
+        }
+
+        public do(callback: PacketReceiver) {
+            for (let sub of this.m_subs) {
+                this.m_manager.set(this.m_main, sub, callback);
+            }
+        }
+
+        public ignore() {
+            for (let sub of this.m_subs) {
+                this.m_manager.unset(this.m_main, sub);
             }
         }
     }
-}
 
-interface ResolverStateWrapper {
-    listener: StateListener;
-}
+    class ResolverProperty {
+        private m_manager: ResolverManager;
+        private m_openListener: ResolverStateWrapper;
+        private m_closeListener: ResolverStateWrapper;
 
-class ResolverStateBinder {
-    private m_stateListener: ResolverStateWrapper;
+        constructor(manager: ResolverManager, openListener: ResolverStateWrapper, closeListener: ResolverStateWrapper) {
+            this.m_manager = manager;
+            this.m_openListener = openListener;
+            this.m_closeListener = closeListener;
+        }
 
-    constructor(stateListener: ResolverStateWrapper) {
-        this.m_stateListener = stateListener;
-    }
+        public system(sub: number | number[]) {
+            return new ResolverBinder(this.m_manager, 0x00, this.subArray(sub));
+        }
 
-    public do(callback: StateListener) {
-        this.m_stateListener.listener = callback;
-    }
+        public game(sub: number | number[]) {
+            return new ResolverBinder(this.m_manager, 0x01, this.subArray(sub));
+        }
 
-    public ignore() {
-        this.m_stateListener.listener = () => { };
-    }
-}
+        public debug(sub: number | number[]) {
+            return new ResolverBinder(this.m_manager, 0x02, this.subArray(sub));
+        }
 
-class ResolverBinder {
-    private m_manager: ResolverManager;
-    private m_main: number;
-    private m_subs: number[];
+        public get open() {
+            return new ResolverStateBinder(this.m_openListener);
+        }
 
-    constructor(manager: ResolverManager, main: number, subs: number[]) {
-        this.m_manager = manager;
-        this.m_main = main;
-        this.m_subs = subs;
-    }
+        public get close() {
+            return new ResolverStateBinder(this.m_closeListener);
+        }
 
-    public do(callback: PacketReceiver) {
-        for (let sub of this.m_subs) {
-            this.m_manager.set(this.m_main, sub, callback);
+        private subArray(raw: number | number[]) {
+            if (raw instanceof Array) {
+                return <number[]>raw;
+            } else {
+                return [<number>raw];
+            }
         }
     }
 
-    public ignore() {
-        for (let sub of this.m_subs) {
-            this.m_manager.unset(this.m_main, sub);
-        }
-    }
-}
+    export class Socket {
+        private m_socket: WebSocket;
+        private m_resolverManager: ResolverManager;
+        private m_open: boolean = false;
+        private m_openListener: ResolverStateWrapper;
+        private m_closeListener: ResolverStateWrapper;
 
-class ResolverProperty {
-    private m_manager: ResolverManager;
-    private m_openListener: ResolverStateWrapper;
-    private m_closeListener: ResolverStateWrapper;
+        constructor(url: string) {
+            this.m_resolverManager = new ResolverManager();
+            this.m_socket = new WebSocket(url);
+            this.m_socket.binaryType = "arraybuffer";
+            this.m_openListener = { listener: () => { } };
+            this.m_closeListener = { listener: () => { } };
 
-    constructor(manager: ResolverManager, openListener: ResolverStateWrapper, closeListener: ResolverStateWrapper) {
-        this.m_manager = manager;
-        this.m_openListener = openListener;
-        this.m_closeListener = closeListener;
-    }
+            this.m_socket.onmessage = event => {
+                let data = <ArrayBuffer>event.data;
+                let view = new DataView(data);
+                let header = view.getUint16(0);
 
-    public system(sub: number | number[]) {
-        return new ResolverBinder(this.m_manager, 0x00, this.subArray(sub));
-    }
+                let main = (header & 0xF000) >> 12;
+                let sub = header & 0x0FFF;
+                this.m_resolverManager.call(main, sub, { data: data });
+            }
 
-    public game(sub: number | number[]) {
-        return new ResolverBinder(this.m_manager, 0x01, this.subArray(sub));
-    }
+            this.m_socket.onopen = () => {
+                this.m_open = true;
+                this.m_openListener.listener();
+            }
 
-    public debug(sub: number | number[]) {
-        return new ResolverBinder(this.m_manager, 0x02, this.subArray(sub));
-    }
-
-    public get open() {
-        return new ResolverStateBinder(this.m_openListener);
-    }
-
-    public get close() {
-        return new ResolverStateBinder(this.m_closeListener);
-    }
-
-    private subArray(raw: number | number[]) {
-        if (raw instanceof Array) {
-            return <number[]>raw;
-        } else {
-            return [<number>raw];
-        }
-    }
-}
-
-class Socket {
-    private m_socket: WebSocket;
-    private m_resolverManager: ResolverManager;
-    private m_open: boolean = false;
-    private m_openListener: ResolverStateWrapper;
-    private m_closeListener: ResolverStateWrapper;
-
-    constructor(url: string) {
-        this.m_resolverManager = new ResolverManager();
-        this.m_socket = new WebSocket(url);
-        this.m_socket.binaryType = "arraybuffer";
-        this.m_openListener = { listener: () => { } };
-        this.m_closeListener = { listener: () => { } };
-
-        this.m_socket.onmessage = event => {
-            let data = <ArrayBuffer>event.data;
-            let view = new DataView(data);
-            let header = view.getUint16(0);
-
-            let main = (header & 0xF000) >> 12;
-            let sub = header & 0x0FFF;
-            this.m_resolverManager.call(main, sub, { data: data });
+            this.m_socket.onclose = () => {
+                this.m_open = false;
+                this.m_closeListener.listener();
+            }
         }
 
-        this.m_socket.onopen = () => {
-            this.m_open = true;
-            this.m_openListener.listener();
+        public send(data: Binary) {
+            this.m_socket.send(data);
         }
 
-        this.m_socket.onclose = () => {
-            this.m_open = false;
-            this.m_closeListener.listener();
+        public get on() {
+            return new ResolverProperty(this.m_resolverManager,
+                this.m_openListener, this.m_closeListener);
         }
-    }
-
-    public send(data: Binary) {
-        this.m_socket.send(data);
-    }
-
-    public get on() {
-        return new ResolverProperty(this.m_resolverManager,
-            this.m_openListener, this.m_closeListener);
     }
 }
