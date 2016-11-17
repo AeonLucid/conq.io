5e59e3dde6d586d024ba63c07c483589f5ccdeb5
Shashi Chotkan <ssr.dev@hotmail.com>
2016-11-17 02:05:56 +0100

    Add basic object serializer and type validator
    As many operations will not be send frequently and are not too large,
    there is no really need for advanced compression strategy and smart binary
    manipulation. A simple BSON serializer will fit for such packets.
    
    The interface is a wrapper around the BSON serializer. It also includes a
    simple type validator, to make sure that the packet is valid. It is also a
    consequence for the fact that the client packets never should be trusted.

---
+++ src/public/basic-serializer.ts
@@ -0,0 +1,125 @@
+namespace bser {
+	declare function bson(): any;
+
+	function validateObject(check: any, object: any, level: number): boolean {
+		const type = "object";
+		if (typeof check !== type || typeof object !== type)
+			return false;
+
+		let checkKeys = Object.keys(check);
+		let objectKeys = Object.keys(object);
+
+		if (checkKeys.length !== objectKeys.length)
+			return false;
+		let count = objectKeys.length;
+
+		for (let index = 0; index < count; index++) {
+			let checkElement = check[checkKeys[index]];
+			let objectElement = object[objectKeys[index]];
+			let good = validatePrimitive(checkElement, objectElement, level + 1) ||
+				validateArray(checkElement, objectElement, level + 1) || validateObject(checkElement, objectElement, level + 1);
+			if (!good)
+				return false;
+		}
+
+		return true;
+	}
+
+	function validatePrimitive(check: any, object: any, level: number): boolean {
+		let checkType = typeof check;
+		let objectType = typeof object;
+
+		const primitives = new Set<string>(["boolean", "number", "string"]);
+
+		if (!primitives.has(checkType))
+			return false;
+
+		let good = checkType === objectType;
+		return good;
+	}
+
+	function validateArray(check: any, object: any, level: number): boolean {
+		const type = "object";
+		if (typeof check !== type || typeof object !== type)
+			return false;
+		if (!(check instanceof Array && object instanceof Array))
+			return false;
+
+		let checkArray = <any[]>check;
+		let objectArray = <any[]>object;
+
+		if (checkArray.length < 0 || objectArray.length < 0)
+			return false;
+		if (checkArray.length === 0 && objectArray.length !== 0)
+			return false;
+
+		let count = objectArray.length;
+
+		for (let index = 0; index < count; index++) {
+			let checkElement = checkArray[0];
+			let objectElement = objectArray[index];
+			let good = validatePrimitive(checkElement, objectElement, level + 1) ||
+				validateArray(checkElement, objectElement, level + 1) || validateObject(checkElement, objectElement, level + 1);
+
+			if (!good)
+				return false;
+		}
+
+		return true;
+	}
+
+	function toArrayBuffer(buffer: Uint8Array, padding: number = 0): ArrayBuffer {
+		let result = new ArrayBuffer(buffer.length + padding);
+		let view = new Uint8Array(result);
+		for (let i = 0; i < buffer.length; ++i)
+			view[i + padding] = buffer[i];
+		return result;
+	}
+
+	function toNativeBuffer(buffer: ArrayBuffer, padding: number = 0): Uint8Array {
+		let result = new Uint8Array(buffer.byteLength);
+		let view = new Uint8Array(buffer);
+		for (let i = 0; i < buffer.byteLength; ++i)
+			result[i] = view[i + padding];
+		return result;	
+	}
+
+	let bsonp = bson().BSON;
+
+	class Codec<T> {
+		private m_object: T;
+
+		constructor(object: T) {
+			this.m_object = object;
+		}
+
+		public encode(object: T, padding: number = 0): ArrayBuffer {
+			let data = bsonp.serialize(object, false, true, false);
+			return toArrayBuffer(data, padding);
+		}
+
+		public decode(binary: ArrayBuffer, padding: number = 0): T | undefined {
+			try {
+				let buffer = toNativeBuffer(binary, padding);
+				let object = bsonp.deserialize(buffer);
+				if (!object)
+					return undefined
+				if (!this.validate(object))
+					return undefined;
+
+				return <T>object;
+			} catch (err) {
+				console.log(err);
+				return undefined;
+			}
+		}
+
+		private validate(object: any): boolean {
+			return validateObject(this.m_object, object, 0);
+		}
+	}
+
+	export function gen<T>(object: T): Codec<T> {
+		return new Codec<T>(object);
+	}
+}
