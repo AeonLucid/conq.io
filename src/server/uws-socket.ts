594798453bcc9f9ad527847e60ec29e6f869c63f
Shashi Chotkan <ssr.dev@hotmail.com>
2016-11-17 16:09:44 +0100

    Add MicroWS socket handler implementation
    MicroWS is probaly the fastest websocket implementation for the NodeJS
    environment. As the application expect high frequency packet interaction
    between client and server, a fast websocket implementation is a must.
    
    The MicroWS socket handler implementation has been added as a handler for
    the server socket interface.

---
+++ src/server/uws-socket.ts
@@ -0,0 +1,59 @@
+import wsw = require("./socket");
+import http = require("http");
+
+let uws = require("uws");
+
+export class Socket {
+    private m_socket: any;
+    private m_clients: { client: wsw.Client, socket: any }[];
+    private m_handler: wsw.SocketHandler;
+    private static s_uniqueIdentifier = 0;
+
+    constructor(server: http.Server) {
+        this.m_socket = new uws.Server({ server: server });
+        this.m_clients = [];
+
+        this.m_handler = new wsw.SocketHandler(packet => {
+            for (let client of this.m_clients) {
+                if (client.client === packet.client) {
+                    client.socket.send(packet.data);
+                }
+            }
+        });
+
+        this.m_socket.on("connection", socket => {
+            Socket.initSocket(socket);
+            this.m_handler.connect(Socket.getIdentifier(socket));
+            this.m_clients.push({ client: Socket.getIdentifier(socket), socket: socket });
+
+            socket.on("message", packet => {
+                if (packet instanceof ArrayBuffer)
+                    this.m_handler.packet(Socket.getIdentifier(socket), <ArrayBuffer>packet);
+            });
+
+            socket.on("close", () => {
+                this.m_handler.disconnect(Socket.getIdentifier(socket));
+                let client = Socket.getIdentifier(socket);
+                for (let i = this.m_clients.length - 1; i >= 0; i--) {
+                    if (this.m_clients[i].client === client) {
+                        this.m_clients.splice(i, 1);
+                        return;
+                    }
+                }
+                throw new Error("Unexpected error; unregisted client disconnected from the server");
+            });
+        });
+    }
+
+    public get handler(): wsw.SocketHandler {
+        return this.m_handler;
+    }
+
+    private static initSocket(socket: any) {
+        socket.__$identifier = Socket.s_uniqueIdentifier++;
+    }
+
+    private static getIdentifier(socket: any): number {
+        return <number>(socket.__$identifier);
+    }
+}
