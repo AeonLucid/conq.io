d5d712417c61b89cb805c37f0dffa9ed9cc1fc3f
Shashi Chotkan <ssr.dev@hotmail.com>
2016-11-20 19:36:11 +0100

    Make first working game
    This game includes moving around in a smooth way. Please note that the
    game does not pass data to the server, it is completely local.

--- src/public/start.ts
+++ src/public/start.ts
@@ -3,12 +3,94 @@
 let body: HTMLElement = document.body;
 let engine: ConcreteEngine;
 
+let vec2 = mvec.vec2;
+type vec2 = mvec.vec2;
+
+interface Tank {
+	position: vec2;
+	velocity: vec2;
+	name: string;
+	health: number;
+	angle: number;
+}
+
+class ClientKeyboard {
+	private m_input: number;
+
+	constructor(input: number = 0) {
+		this.m_input = input & 0xF;
+	}
+
+	public get left(): boolean {
+		return this.get(0);
+	}
+
+	public get right(): boolean {
+		return this.get(1);
+	}
+
+	public get up(): boolean {
+		return this.get(2);
+	}
+
+	public get down(): boolean {
+		return this.get(3);
+	}
+
+	public set left(value: boolean) {
+		this.put(0, value);
+	}
+
+	public set right(value: boolean) {
+		this.put(1, value);
+	}
+	
+	public set up(value: boolean) {
+		this.put(2, value);
+	}
+
+	public set down(value: boolean) {
+		this.put(3, value);
+	}
+
+	public get binary(): number {
+		return this.m_input & 0xF;
+	}
+
+	private get(k: number): boolean {
+		return ((this.m_input >> k) & 1) === 1 ? true : false;
+	}
+
+	private set(k: number) {
+		this.m_input |= (1 << k);
+	}
+
+	private clear(k: number) {
+		this.m_input &= ~(1 << k);
+	}
+
+	private put(k: number, v: boolean) {
+		if (v) this.set(k); else this.clear(k);
+	}
+}
+
+function getKeyboardInput(arr: Set<number>) {
+	let keys = new ClientKeyboard();
+	keys.left = arr.has(65);
+	keys.right = arr.has(68);
+	keys.up = arr.has(87);
+	keys.down = arr.has(83);
+	return keys;
+}
+
 class ConcreteEngine extends render.BasicEngine {
 	private m_keys: Set<number>;
 	private m_socket: wsw.Socket;
+	private m_user: Tank;
 
 	protected init(g: render.Graphics, window: render.Window): void {
 		this.m_keys = new Set<number>();
+		this.m_user = { position: vec2.zero, velocity: vec2.zero, angle: 0, health: 1, name: "Elsa" };
 		this.m_socket = new wsw.Socket("ws://localhost:3000");
 
 		body.onkeydown = event => {
@@ -20,9 +102,44 @@ class ConcreteEngine extends render.BasicEngine {
 		}
 	}
 
-	protected update(deltatime: number, window: render.Window): void { }
+	protected update(deltatime: number, window: render.Window): void {
+		let keys = getKeyboardInput(this.m_keys);
+
+		engine(this.m_user, deltatime, input(keys));
+
+		function input(input: ClientKeyboard) {
+			let result = vec2.zero;
+			let force = 6.351;
+
+			result.x -= input.left ? 1 : 0;
+			result.x += input.right ? 1 : 0;
+			result.y += input.up ? 1 : 0;
+			result.y -= input.down ? 1 : 0;
+
+			if (result.x === 0 && result.y === 0)
+				return vec2.zero;
+			
+			return vec2.scale(force, vec2.normalize(result));
+		}
+
+		function engine(tank: Tank, deltatime: number, acc: vec2) {
+			let dt = deltatime;
+			let momentum = 0.93;
+
+			tank.velocity = vec2.add(tank.velocity, vec2.scale(dt, acc));
+			tank.position = vec2.add(tank.position, vec2.scale(dt, tank.velocity));
+			tank.velocity = vec2.scale(momentum, tank.velocity);
+		}
+	}
+
 	protected fixedupdate(deltatime: number, window: render.Window): void { }
-	protected render(sg: render.SimpleGraphics, deltatime: number, window: render.Window, vp: render.Viewport): void { }
+	
+	protected render(sg: render.SimpleGraphics, deltatime: number, window: render.Window, vp: render.Viewport): void {
+		sg.stroke(false).fill("#303030").prect(-vp.aspect, -1, 2 * vp.aspect, 2);
+
+		sg.stroke(true).stroke("#2E15A2").fill("#0E15A0").ellipse(this.m_user.position, 0.1);
+	}
+
 	protected rendertext(sg: render.SimpleGraphics, deltatime: number, window: render.Window, vp: render.Viewport): void { }
 }
 
